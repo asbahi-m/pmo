cdb9a0f98bc2dc047e8a5a540474eb0b114072e5
Mohammed Alasbahi <asbahi.mo@gmail.com>
2019-11-18 08:27:18 +0300

    تعديل النافبار + ضبط توافق الرئيسية مع الجوالات

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ $(document).ready(function () {
             $('#backToTop').fadeOut();
         }
     });
-    
     $("#backToTop").click(function(event) {
         event.preventDefault();
         $("html, body").animate({ scrollTop: 0 }, "slow");
         return false;
     });
 
+    /* سلايدر الصور في الرئيسية */
     // external js: flickity.pkgd.js
     // show
     var $carousel = $('#picturesSlider .carousel').removeClass('is-hidden');
@@ -22,4 +22,9 @@ $(document).ready(function () {
     $carousel[0].offsetHeight;
     // init Flickity
     $carousel.flickity();
+
+    /* ضبط مقاس ارتفاع إطارات الفيديو بالتناسب مع عرض الإطار */
+    var iframeV = $("#videoSlider .carousel-inner iframe");
+    iframeV.height($(iframeV).innerWidth()/(16/9));
+    
 });
